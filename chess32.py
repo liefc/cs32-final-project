213e057a514db3f6f680c93c1104169abeaa347c
Collin Lief <clief@college.harvard.edu>
2023-04-21 13:56:18 -0400

    created Piece class and started creating a check for if a player is in check

--- chess32.py
+++ chess32.py
@@ -1,5 +1,6 @@
 class ChessBoard():
     def __init__(self):
+        self.pieces = []
         self.state = [
             ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
@@ -62,6 +63,80 @@ class ChessBoard():
             
         return board
 
+    def in_check(self, player):
+        # Find location of king
+        for rank in range(8):
+            if f'{player}k' in self.state[rank]:
+                loc = (self.state[rank].index(f'{player}k'), rank)
+
+        # Check in each direction if there is a piece that can hit the king
+        # Cardinal directions:
+        for x in range(loc[0]-1, -1, -1):
+            if self.state[x][loc[1]] != '':
+                if self.state[x][loc[1]][0] != player and self.state[x][loc[1]][1] in ['q','r']:
+                    return True
+                break
+        for x in range(loc[0]+1, 8):
+            if self.state[x][loc[1]] != '':
+                if self.state[x][loc[1]][0] != player and self.state[x][loc[1]][1] in ['q','r']:
+                    return True
+                break
+        for y in range(loc[1]-1, -1, -1):
+            if self.state[loc[0]][y] != '':
+                if self.state[loc[0]][y][0] != player and self.state[loc[0]][y][1] in ['q','r']:
+                    return True
+                break
+        for y in range(loc[1]+1, 8):
+            if self.state[loc[0]][y] != '':
+                if self.state[loc[0]][y][0] != player and self.state[loc[0]][y][1] in ['q','r']:
+                    return True
+                break
+            
+        # Diagonals:
+        check_loc = loc
+        while True:
+            check_loc = (check_loc[0] + 1, check_loc[1] + 1)
+            if check_loc[0] == 8 or check_loc[1] == 8:
+                break
+            check_piece = self.state[check_loc[0]][check_loc[1]]
+            if check_piece != '':
+                if check_piece[0] != player and check_piece[1] in ['q','b']:
+                    return True
+                break
+        check_loc = loc
+        while True:
+            check_loc = (check_loc[0] + 1, check_loc[1] - 1)
+            if check_loc[0] == 8 or check_loc[1] == 8:
+                break
+            check_piece = self.state[check_loc[0]][check_loc[1]]
+            if check_piece != '':
+                if check_piece[0] != player and check_piece[1] in ['q','b']:
+                    return True
+                break
+        check_loc = loc
+        while True:
+            check_loc = (check_loc[0] - 1, check_loc[1] + 1)
+            if check_loc[0] == 8 or check_loc[1] == 8:
+                break
+            check_piece = self.state[check_loc[0]][check_loc[1]]
+            if check_piece != '':
+                if check_piece[0] != player and check_piece[1] in ['q','b']:
+                    return True
+                break
+        check_loc = loc
+        while True:
+            check_loc = (check_loc[0] - 1, check_loc[1] - 1)
+            if check_loc[0] == 8 or check_loc[1] == 8:
+                break
+            check_piece = self.state[check_loc[0]][check_loc[1]]
+            if check_piece != '':
+                if check_piece[0] != player and check_piece[1] in ['q','b']:
+                    return True
+                break
+
+        #Pawns:
+        #Knights:
+
     def move(self, player, piece, loc):
         # First do checks to see if move is valid
         # Check if the target square is occupied by own piece
@@ -92,6 +167,22 @@ class ChessBoard():
                     if self.state[loc[0]][y] == f'{player}q':
                         valid_pieces.append((loc[0],y))
                     break
+
+class Piece():
+    def __init__(self, piece, color, loc, board):
+        self.piece = piece
+        self.color = color
+        self.loc = loc
+        self.board = board
+        self.moves = []
+        self.check_moves()
+        
+    def check_moves(self, in_check):
+        
+        if self.piece == 'q':
+            for x in range(self.loc[0]-1, -1. -1):
+                if self.board.state[x][loc[1]] != '':
+                    if self.board.state[x][loc[1]][0] != self.color:
 
 def main():
     board = ChessBoard()
